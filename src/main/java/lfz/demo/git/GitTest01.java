093a6c3e4c40c510985366be2598538a84d84b34
lfz <1915565819@qq.com>
2023-05-05 09:49:53 +0800

    proxy commit

--- src/main/java/lfz/demo/git/GitTest01.java
+++ src/main/java/lfz/demo/git/GitTest01.java
@@ -11,5 +11,6 @@ public class GitTest01 {
         System.out.println("push test!  ");
         System.out.println("pull test!  ");
         System.out.println("push test!2  ");
+        System.out.println("代理测试");
     }
 }
